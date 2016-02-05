110af6f5ed31675a3c0a24517942165d08b4c841
Chris L <chris.lichter@gmail.com>
2016-02-04 22:16:50 -0600

    DB working and pulling values with ORM query

--- item_catalog/app/records/record_controllers.py
+++ item_catalog/app/records/record_controllers.py
@@ -17,7 +17,11 @@ from app.records.record_forms import LoginForm
 # Import module models (i.e. User)
 from app.records.record_model import Genre, Artist, Record
 
+engine = db.create_engine('sqlite:///app.db')
+db.metadata.bind = engine
 
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 recordBase = Blueprint('auth', __name__, url_prefix='')
@@ -25,7 +29,8 @@ recordBase = Blueprint('auth', __name__, url_prefix='')
 # Set the route and accepted methods
 @recordBase.route('/', methods=['GET', 'POST'])
 def showRecords():
-    records = Record.query.all()
-    print records
+    
+    records = session.query(Record.title, Record.year, Record.description, Artist.name, Genre.name).join(Artist).join(Genre)
+    print records[0].keys()
 
     return render_template("records/welcome.html", records = records)
