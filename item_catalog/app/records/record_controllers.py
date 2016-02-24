832a484947c2b2c1e4b4084cb6255aff999edf57
Chris Lichter <chris.lichter@gmail.com>
2016-02-24 15:58:51 -0600

    started creating the next modules

--- item_catalog/app/records/record_controllers.py
+++ item_catalog/app/records/record_controllers.py
@@ -8,6 +8,9 @@ from sqlalchemy.orm import sessionmaker
 # Import password / encryption helper tools
 from werkzeug import check_password_hash, generate_password_hash
 
+#json for jsonify
+import json
+
 # Import the database object from the main app module
 from app import db
 
@@ -30,12 +33,17 @@ recordBase = Blueprint('auth', __name__, url_prefix='')
 @recordBase.route('/', methods=['GET', 'POST'])
 def showRecords():
     
-
     records = db.session.query(Record.id, Record.title, Record.year, Record.description, Artist.artist_name, 
         Genre.genre_name).join(Artist).join(Genre)
     print records
 
     return render_template("records/welcome.html", records = records)
+
+@recordBase.route('/JSON', methods=['GET'])
+def showRecordsJSON():
+    records = db.session.query(Record.id, Record.title, Record.year, Record.description, Artist.artist_name, 
+        Genre.genre_name).join(Artist).join(Genre)
+    return jsonify(records=[r.serialize for r in records])
 
 @recordBase.route('/<int:record_id>/', methods=['GET', 'POST'])
 def showRecordInfo(record_id):
@@ -44,3 +52,9 @@ def showRecordInfo(record_id):
     print record
 
     return render_template("records/record_info.html", record = record)
+
+@recordBase.route('/<int:record_id>/JSON', methods=['GET'])
+def showRecordsJSON():
+    record = db.session.query(Record.id, Record.title, Record.year, Record.description, Artist.artist_name, 
+        Genre.genre_name).filter_by(id=record_id).join(Artist).join(Genre).one()
+    return jsonify(record=[r.serialize for r in record])
