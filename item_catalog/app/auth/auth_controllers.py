c450621c972582698bf4e1d90ec3da4b3dd01f11
Chris L <chris.lichter@gmail.com>
2016-03-30 23:48:55 -0500

    auth to big G is good to go

--- item_catalog/app/auth/auth_controllers.py
+++ item_catalog/app/auth/auth_controllers.py
@@ -29,6 +29,28 @@ CLIENT_ID = json.loads(open('config/client_secrets.json', 'r').read())['web']['c
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 authBase = Blueprint('auth', __name__, url_prefix='')
 
+# User Helper Functions
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    db.session.add(newUser)
+    db.session.commit()
+    user = db.session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = db.session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = db.session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
 # Set the route and accepted methods
 @authBase.route('/login', methods=['GET'])
 def login():
